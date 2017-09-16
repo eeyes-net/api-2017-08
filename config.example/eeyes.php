926dc6cd2b26227322ec1b717583f76ab082841b
Ganlv <ganlvtech@qq.com>
2017-09-16 12:18:24 +0800

    添加config/database.php文件

--- config.example/eeyes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'admin' => [
-        'url' => 'http://admin.eeyes.net',
-    ],
-];
---
+++ config/database.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'api'),
+            'username' => env('DB_USERNAME', 'api'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => 'MyISAM',
+        ],
+
+        'mysql_permission' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_PERMISSION_DATABASE', 'permission'),
+            'username' => env('DB_PERMISSION_USERNAME', 'permission'),
+            'password' => env('DB_PERMISSION_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => 'MyISAM',
+        ],
+    ],
+
+    'migrations' => 'migrations',
+
+    'redis' => [
+        'client' => 'predis',
+        'default' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 0,
+        ],
+    ],
+];
